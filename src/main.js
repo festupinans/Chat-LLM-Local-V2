16be0554f827fae099db20316f010251da485fe0
Francisco Estupiñán <90729579+festupinans@users.noreply.github.com>
2025-05-15 23:44:37 -0500

    Limpieza Main.JS

--- src/main.js
+++ src/main.js
@@ -2,15 +2,4 @@ import './CSS/style.css';
 import './CSS/Three.css';
 import { initScene } from './ThreeJS/Scene.js';
 
-// Inicializamos la escena de Three.js
 initScene();
-
-// document.getElementById('senBtn').addEventListener('click', () => {
-//     sendMensajeIA();
-// });
-
-// document.getElementById('clearBtn').addEventListener('click', () => {
-//     document.getElementById('transcript').value = '';
-// });
-
-
